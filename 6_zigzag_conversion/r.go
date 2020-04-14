4ff29c1ff6fb2a644cc4e6d7019f66dd7afc2388
Alexey Lazarev <lazarev.alexey@eldorado.ru>
2020-04-14 21:10:54 +0300

    6. ZigZag Conversion solution

--- 6_zigzag_conversion/r.go
+++ 6_zigzag_conversion/r.go
@@ -6,5 +6,29 @@ func convert(s string, numRows int) string {
 	if length <= numRows {
 		return s
 	}
+
+	array := make([]string, numRows)
+
+	for index, row := 0, 0; index < length; {
+		if row < numRows {
+			array[row] += string(s[index])
+			row++
+			index++
+			continue
+		}
+
+		for subRow := numRows - 1; subRow > 0 && index < length; subRow-- {
+			if subRow == numRows-1 {
+				continue
+			}
+			array[subRow] += string(s[index])
+			index++
+		}
+		row = 0
+	}
+
+	for _, substring := range array {
+		result += substring
+	}
 	return result
 }
