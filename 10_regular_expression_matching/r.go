6c26a393d63d9c8a02fafd093be60af9c1947b11
Alexey Lazarev <lazarev.alexey@eldorado.ru>
2020-04-20 23:42:31 +0300

    10. Regular Expression Matching solution

--- 10_regular_expression_matching/r.go
+++ 10_regular_expression_matching/r.go
@@ -1,5 +1,49 @@
 package _0_regular_expression_matching
 
 func isMatch(s string, p string) bool {
-	return false
+	lengthP := len(p)
+	lengthS := len(s)
+	if lengthP == 0 {
+		return lengthS == 0
+	}
+	s0 := uint8(0)
+	if lengthS > 0 {
+		s0 = s[0]
+	}
+
+	//*
+	if p[0] == 42 {
+		return false
+	}
+	result := false
+
+	//a-z || .
+	if p[0] >= 97 && p[0] <= 122 || p[0] == 46 {
+		if s0 == p[0] || p[0] == 46 {
+			if lengthP > 1 && p[1] == 42 {
+				if lengthS == 0 && lengthP > 1 {
+					result = isMatch(sub(s, 1), sub(p, 2))
+				} else {
+					result = isMatch(sub(s, 1), sub(p, 0)) ||
+						isMatch(sub(s, 0), sub(p, 2))
+				}
+			} else {
+				result = s0 != 0 && isMatch(sub(s, 1), sub(p, 1))
+			}
+		} else {
+			if lengthP > 1 && p[1] == 42 {
+				result = isMatch(sub(s, 0), sub(p, 2))
+			}
+		}
+	}
+
+	return result
+}
+
+func sub(s string, leftIndex uint) string {
+	if len(s) < int(leftIndex)+1 {
+		return ""
+	} else {
+		return s[leftIndex:]
+	}
 }
--- 10_regular_expression_matching/r_test.go
+++ 10_regular_expression_matching/r_test.go
@@ -16,8 +16,83 @@ func (t *testCases) Add(tc testCase) {
 	t.Tc = append(t.Tc, tc)
 }
 
+type testCase2 struct {
+	Input1 string
+	Input2 uint
+	Output string
+}
+
+type testCases2 struct {
+	Tc []testCase2
+}
+
+func (t *testCases2) Add(tc testCase2) {
+	t.Tc = append(t.Tc, tc)
+}
+
+func TestSub(t *testing.T) {
+	tc := testCases2{}
+	tc.Add(testCase2{
+		Input1: "aa",
+		Input2: 1,
+		Output: "a",
+	})
+	tc.Add(testCase2{
+		Input1: "aa",
+		Input2: 0,
+		Output: "aa",
+	})
+	tc.Add(testCase2{
+		Input1: "aa",
+		Input2: 2,
+		Output: "",
+	})
+	tc.Add(testCase2{
+		Input1: "aa",
+		Input2: 3,
+		Output: "",
+	})
+
+	tc.Add(testCase2{
+		Input1: "ab",
+		Input2: 1,
+		Output: "b",
+	})
+	tc.Add(testCase2{
+		Input1: "abc",
+		Input2: 1,
+		Output: "bc",
+	})
+
+	for _, c := range tc.Tc {
+		if result := sub(c.Input1, c.Input2); result != c.Output {
+			t.Fatalf("sub(%v, %v) != %v, expectrd = %v", c.Input1, c.Input2, result, c.Output)
+		}
+	}
+}
+
 func TestIsMatch(t *testing.T) {
 	tc := testCases{}
+	tc.Add(testCase{
+		Input1: "a",
+		Input2: ".*..a*",
+		Output: false,
+	})
+	tc.Add(testCase{
+		Input1: "bbbba",
+		Input2: ".*a*a",
+		Output: true,
+	})
+	tc.Add(testCase{
+		Input1: "a",
+		Input2: "ab*",
+		Output: true,
+	})
+	tc.Add(testCase{
+		Input1: "i",
+		Input2: ".",
+		Output: true,
+	})
 	tc.Add(testCase{
 		Input1: "aa",
 		Input2: "a",
@@ -43,10 +118,26 @@ func TestIsMatch(t *testing.T) {
 		Input2: "mis*is*p*.",
 		Output: false,
 	})
+	tc.Add(testCase{
+		Input1: "mississippi",
+		Input2: "mis*is*ip*.",
+		Output: true,
+	})
+
+	tc.Add(testCase{
+		Input1: "aaa",
+		Input2: "a*a",
+		Output: true,
+	})
+	tc.Add(testCase{
+		Input1: "aaa",
+		Input2: "ab*a*c*a",
+		Output: true,
+	})
 
 	for _, c := range tc.Tc {
 		if result := isMatch(c.Input1, c.Input2); result != c.Output {
-			t.Fatalf("isMatch(%v,%v) != %v, expectrd = %v", c.Input1, c.Input2, result, c.Output)
+			t.Fatalf("isMatch(%v, %v) != %v, expectrd = %v", c.Input1, c.Input2, result, c.Output)
 		}
 	}
 }
